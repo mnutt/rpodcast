7775b79886bb42ba931d47112910935b2c759046
Michael Nutt <michael@nuttnet.net>
2008-10-13 12:41:39 -0400

    Don't fail on enclosure parsing problems

--- lib/rpodcast/enclosure.rb
+++ lib/rpodcast/enclosure.rb
@@ -3,9 +3,9 @@ module RPodcast
     attr_accessor :url, :type, :size
     
     def initialize(element)
-      @url = element['url']
-      @type = element['type']
-      @size = (element['size'] or element['length']).to_i
+      @url = element['url'] rescue nil
+      @type = element['type'] rescue nil
+      @size = (element['size'] or element['length']).to_i rescue nil
     end
   end
 end
